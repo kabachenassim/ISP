6d05df260b375f7d72d59dabef66bd9d04acae2b
kabache <nassim199206@gmail.com>
2021-03-25 13:46:48 +0100

    version final

---
+++ src/main/java/fr/uvsq/poo/ISP/copy.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uvsq.poo.ISP;
+
+/**
+ *
+ * @author ADMIN
+ */
+public interface copy {
+    public void copy();
+    
+}
